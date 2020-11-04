215ecf3704145ef3c2a8f29851443c6e724b346f
cnsumner <cnsumner@users.noreply.github.com>
2020-11-04 16:17:43 -0500

    Check if pubspec.yaml exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 PUBSPEC_PATH="${1-.}"
-echo "Reading package version from ${PUBSPEC_PATH}/pubspec.yaml"
+if test -f "${PUBSPEC_PATH}/pubspec.yaml"; then
+    echo "Reading package version from ${PUBSPEC_PATH}/pubspec.yaml"
+else
+    exit 1
+fi
+
 PACKAGE_VERSION=$(cat ${PUBSPEC_PATH}/pubspec.yaml | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 PACKAGE_VERSION_WITH_V="v${PACKAGE_VERSION}"
 
